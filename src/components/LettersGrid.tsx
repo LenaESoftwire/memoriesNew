0d4ec28feacd4a24e48b590026a641e8eb3e6557
Lena E <elena.evdokimova@softwire.com>
2021-08-18 19:19:16 +0100

    Logic is working

--- src/components/LettersGrid.tsx
+++ src/components/LettersGrid.tsx
@@ -1,17 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styling/LettersGrid.css";
 
 export default function LettersGrid(): JSX.Element {
-    const lettersList = ["M", "E", "O", "R", "I", "S", 
-                        "E", "M", "O", "S", "I", "R"];
-    
-    const[chosen, setChosen] = useState<number>();
+    const lettersList = ["M", "E", "O", "R", "I", "S",
+        "E", "M", "O", "S", "I", "R"];
+
+    const [first, setFirst] = useState<number>(-1);
+    const [second, setSecond] = useState<number>(-1);
+    const [out, setOut] = useState<number[]>([-1]);
+
+    function handleClick(index: number) {
+        if (!out.includes(index)) {
+            if (first === -1) {
+                setFirst(index);
+            }
+            else if (second === -1 && index !== first) {
+                setSecond(index);
+            }
+            else if (second === index || first === index) {
+                setFirst(-1);
+                setSecond(-1);
+            }
+            else {
+                setFirst(index);
+                setSecond(-1);
+            }
+        }
+
+    }
+
+    useEffect(() => {
+        if (first !== -1 && second !== -1) {
+            if (lettersList[first] === lettersList[second]) {
+                setOut(out.concat([first, second]))
+            }
+        }
+    }, [first, second])
 
     const letters = lettersList.map((l, index) =>
-        <div className={chosen === index ? "letter" : "letter-closed"} key={index}
-        onClick={() => setChosen(index)}>{l}</div>);
+        <div className={out.includes(index) ? "out" : first === index || second === index ? "letter" : "letter-closed"} key={index}
+            onClick={() => handleClick(index)}>{l}</div>);
 
-    // const 
 
     return (
         <div className="letters">
