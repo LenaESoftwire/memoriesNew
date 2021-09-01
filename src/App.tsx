ebb308b48f6bbc03dd033b0ebcb2a30e50aa136e
Lena E <elena.evdokimova@softwire.com>
2021-09-02 00:24:56 +0100

    keeping score

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import LettersGrid from './components/LettersGrid';
 
+export interface Step {
+  step: number;
+}
+
 export default function App(): JSX.Element {
+
+  const [step, setStep] = useState<Step>({step: 0});
+
   return (
     <div className="App">
-      <h1>Memories</h1>
-      <LettersGrid></LettersGrid>
+      <h1>Memories Step {step.step} </h1>
+      {console.log(`step: ${step.step}`)}
+      <LettersGrid step={step} setStep={setStep}></LettersGrid>
     </div>
   );
 }
--- src/components/LettersGrid.tsx
+++ src/components/LettersGrid.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import "../styling/LettersGrid.css";
 import { shuffleArray } from "./ShuffleArray";
+import { Step } from "../App"
 
-export default function LettersGrid(): JSX.Element {
+interface LetterGridProps {
+    step: Step;
+    setStep: Dispatch<SetStateAction<Step>>;
+}
+
+export default function LettersGrid({ step, setStep }: LetterGridProps): JSX.Element {
 
     const lettersListOrdered = ["M", "E", "O", "R", "I", "S",
         "E", "M", "O", "S", "I", "R"];
@@ -10,7 +16,10 @@ export default function LettersGrid(): JSX.Element {
     const [first, setFirst] = useState<number>(-1);
     const [second, setSecond] = useState<number>(-1);
     const [out, setOut] = useState<number[]>([-1]);
-    const [lettersList, setLettersList] = useState<string[]>([""]);
+    const [lettersList] = useState<string[]>(shuffleArray(lettersListOrdered));
+    const [player, setPlayer] = useState<number>(-1);
+    const [score1, setScore1] = useState<number>(0);
+    const [score2, setScore2] = useState<number>(0);
 
     function handleClick(index: number) {
         if (!out.includes(index)) {
@@ -19,6 +28,8 @@ export default function LettersGrid(): JSX.Element {
             }
             else if (second === -1 && index !== first) {
                 setSecond(index);
+                setStep({ step: step.step++ });
+                { console.log(`that step: ${step.step}`) }
             }
             else if (second === index || first === index) {
                 setFirst(-1);
@@ -27,18 +38,29 @@ export default function LettersGrid(): JSX.Element {
             else {
                 setFirst(index);
                 setSecond(-1);
+                setStep({ step: step.step++ });
+                { console.log(`this step: ${step.step}`) }
             }
         }
     }
 
-    useEffect(() => {
-        setLettersList (shuffleArray(lettersListOrdered));
-    }, []);
+    // useEffect(() => {
+    //     setLettersList (shuffleArray(lettersListOrdered));
+    // }, []);
 
     useEffect(() => {
         if (first !== -1 && second !== -1) {
             if (lettersList[first] === lettersList[second]) {
-                setOut(out.concat([first, second]))
+                setOut(out.concat([first, second]));
+                if (player === -1) {
+                    setScore1(score1+1);
+                }
+                else {
+                    setScore2(score2+1);
+                }
+            }
+            else {
+                setPlayer(-player);
             }
         }
     }, [first, second])
@@ -48,8 +70,14 @@ export default function LettersGrid(): JSX.Element {
             onClick={() => handleClick(index)}>{l}</div>);
 
     return (
-        <div className="letters">
-            {letters}
+        <div className="game">
+            <div className="players">
+                <div className={player === -1 ? "active" :"player"}>Player1 {score1} </div>
+                <div className={player === 1 ? "active" :"player"}>Player2 {score2} </div>
+            </div>
+            <div className="letters">
+                {letters}
+            </div>
         </div>
 
     );
